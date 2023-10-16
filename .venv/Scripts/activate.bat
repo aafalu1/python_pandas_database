245376d080bce484ea7eac3a5e0188edd756f7cf
Basanta Bhandari <basanta17@yahoo.com>
2023-10-15 20:16:57 -0500

    file updated

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Basanta\python_worspaces\.venv
+set VIRTUAL_ENV=D:\Basanta\my_project\python_pandas_database\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- oracle_db/database_utill.py
+++
@@ -1,2 +0,0 @@
-import oracle_db
-
